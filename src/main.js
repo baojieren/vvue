1760ae5ba4ab7a6a49e684623a75e45f598c7bc0
renbaojie <2013142083@qq.com>
2019-12-13 14:34:44 +0800

    vuex啊

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import axios from './utils/http'
 import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
 import '@/assets/globalCss.css'
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.use(ViewUI);
@@ -12,5 +13,6 @@ Vue.prototype.axios = axios;
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app');
