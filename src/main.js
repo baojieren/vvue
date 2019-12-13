93d96bd581778a14bb3d4b32af52de527733a12a
renbaojie <2013142083@qq.com>
2019-12-13 14:47:30 +0800

    vuex初始化

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import 'view-design/dist/styles/iview.css'
 import '@/assets/globalCss.css'
 import store from './store'
 
+// 全局组件
+// import globalComponent from '@/utils/globalComponent'
+// Vue.use(globalComponent);
+
 Vue.config.productionTip = false;
 Vue.use(ViewUI);
 Vue.prototype.axios = axios;
